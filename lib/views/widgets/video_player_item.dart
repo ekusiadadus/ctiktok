63b09e0b8b7f609b5c637cb229faa8b5711e3d20
ekusiadadus <ekusiadadus@gmail.com>
2022-11-07 01:05:34 +0900

    refactor: :window: change the size of video item

--- lib/views/widgets/video_player_item.dart
+++ lib/views/widgets/video_player_item.dart
@@ -3,8 +3,10 @@ import 'package:video_player/video_player.dart';
 
 class VideoPlayerItem extends StatefulWidget {
   final String videoUrl;
-
-  const VideoPlayerItem({Key? key, required this.videoUrl}) : super(key: key);
+  const VideoPlayerItem({
+    Key? key,
+    required this.videoUrl,
+  }) : super(key: key);
 
   @override
   _VideoPlayerItemState createState() => _VideoPlayerItemState();
@@ -17,14 +19,10 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
   void initState() {
     super.initState();
     videoPlayerController = VideoPlayerController.network(widget.videoUrl)
-      ..initialize().then((_) {
+      ..initialize().then((value) {
         videoPlayerController.play();
         videoPlayerController.setVolume(1);
       });
-    videoPlayerController.initialize().then((_) {
-      setState(() {});
-    });
-    videoPlayerController.play();
   }
 
   @override
@@ -36,9 +34,10 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
+
     return Container(
       width: size.width,
-      height: size.width,
+      height: size.height,
       decoration: const BoxDecoration(
         color: Colors.black,
       ),
